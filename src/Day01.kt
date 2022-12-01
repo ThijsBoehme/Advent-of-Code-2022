49fb5edecb79e24bedbeb49d7c63d4ad87d09c4e
Thijs Boehme <thijsboehme@me.com>
2022-12-01 09:48:47 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import java.io.File
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+    fun sumOfMostWeightCarriedByNElves(input: String, n: Int) = input
+        .split("\n\n")
+        .map { it.split("\n").map { calories -> calories.toInt() } }
+        .sortedByDescending { it.sum() }
+        .take(n)
+        .sumOf { it.sum() }
+
+    fun part1(input: String) = sumOfMostWeightCarriedByNElves(input, 1)
+
+    fun part2(input: String) = sumOfMostWeightCarriedByNElves(input, 3)
+
+    fun readInput(name: String) = File("src", "$name.txt").readText().trim()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
