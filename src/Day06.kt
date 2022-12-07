c4209563af4cd2dd9e0a7dc3778ea472a82944eb
Thijs Boehme <thijsboehme@me.com>
2022-12-07 10:07:28 +0100

    Day06

---
+++ src/Day06.kt
@@ -0,0 +1,33 @@
+fun main() {
+    fun findMarker(input: String, markerSize: Int): Int {
+        val marker = input.windowed(markerSize)
+            .first { chunk ->
+                chunk.toSet().size == markerSize
+            }
+        return input.indexOf(marker) + markerSize
+    }
+
+    fun part1(input: String): Int {
+        return findMarker(input, 4)
+    }
+
+    fun part2(input: String): Int {
+        return findMarker(input, 14)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part1("nppdvjthqldpwncqszvftbrmjlhg") == 6)
+    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
+    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
+    check(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    check(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
+
+    val input = readInput("Day06").first()
+    println(part1(input))
+    println(part2(input))
+}
